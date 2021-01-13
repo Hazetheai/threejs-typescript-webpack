7ac6e22603d7058dae47e56313be4716f9c16284
Jake <jacobstefan92@gmail.com>
2021-01-13 21:35:41 +0100

    initial commit

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,58 +1,167 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import Stats from 'three/examples/jsm/libs/stats.module'
-import {GUI} from 'three/examples/jsm/libs/dat.gui.module'
+// If using Module Specifiers
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import Stats from "three/examples/jsm/libs/stats.module";
+import { GUI } from "three/examples/jsm/libs/dat.gui.module";
 
-const scene: THREE.Scene = new THREE.Scene()
+const scene: THREE.Scene = new THREE.Scene();
 
-const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
 
-const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+const light = new THREE.PointLight(0xffffff, 1);
+light.position.set(5, 5, 5);
+scene.add(light);
 
-const controls = new OrbitControls(camera, renderer.domElement)
+const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
-const geometry: THREE.BoxGeometry = new THREE.BoxGeometry()
-const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
+const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const cube: THREE.Mesh = new THREE.Mesh(geometry, material)
-scene.add(cube)
+const controls = new OrbitControls(camera, renderer.domElement);
 
-camera.position.z = 2
+const boxGeometry: THREE.BoxGeometry = new THREE.BoxGeometry();
+const sphereGeometry: THREE.SphereGeometry = new THREE.SphereGeometry();
+const icosahedronGeometry: THREE.IcosahedronGeometry = new THREE.IcosahedronGeometry(
+  1,
+  0
+);
+const planeGeometry: THREE.PlaneGeometry = new THREE.PlaneGeometry();
+const torusKnotGeometry: THREE.TorusKnotGeometry = new THREE.TorusKnotGeometry();
 
-window.addEventListener('resize', onWindowResize, false)
+const threeTone = new THREE.TextureLoader().load("img/threeTone.jpg");
+threeTone.minFilter = THREE.NearestFilter;
+threeTone.magFilter = THREE.NearestFilter;
+
+const fourTone = new THREE.TextureLoader().load("img/fourTone.jpg");
+fourTone.minFilter = THREE.NearestFilter;
+fourTone.magFilter = THREE.NearestFilter;
+
+const fiveTone = new THREE.TextureLoader().load("img/fiveTone.jpg");
+fiveTone.minFilter = THREE.NearestFilter;
+fiveTone.magFilter = THREE.NearestFilter;
+
+const material: THREE.MeshToonMaterial = new THREE.MeshToonMaterial();
+
+const cube: THREE.Mesh = new THREE.Mesh(boxGeometry, material);
+cube.position.x = 5;
+scene.add(cube);
+
+const sphere: THREE.Mesh = new THREE.Mesh(sphereGeometry, material);
+sphere.position.x = 3;
+scene.add(sphere);
+
+const icosahedron: THREE.Mesh = new THREE.Mesh(icosahedronGeometry, material);
+icosahedron.position.x = 0;
+scene.add(icosahedron);
+
+const plane: THREE.Mesh = new THREE.Mesh(planeGeometry, material);
+plane.position.x = -2;
+scene.add(plane);
+
+const torusKnot: THREE.Mesh = new THREE.Mesh(torusKnotGeometry, material);
+torusKnot.position.x = -5;
+scene.add(torusKnot);
+
+camera.position.z = 5;
+
+window.addEventListener("resize", onWindowResize, false);
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  render();
 }
 
-const stats = Stats()
-document.body.appendChild(stats.dom)
+const stats = Stats();
+document.body.appendChild(stats.dom);
 
-const gui = new GUI()
-const cubeFolder = gui.addFolder("Cube")
-cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2, 0.01)
-cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2, 0.01)
-cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2, 0.01)
-cubeFolder.open()
-const cameraFolder = gui.addFolder("Camera")
-cameraFolder.add(camera.position, "z", 0, 10, 0.01)
-cameraFolder.open()
+var options = {
+  side: {
+    FrontSide: THREE.FrontSide,
+    BackSide: THREE.BackSide,
+    DoubleSide: THREE.DoubleSide,
+  },
+  gradientMap: {
+    Default: null,
+    threeTone: "threeTone",
+    fourTone: "fourTone",
+    fiveTone: "fiveTone",
+  },
+};
+const gui = new GUI();
+
+var data = {
+  lightColor: light.color.getHex(),
+  color: material.color.getHex(),
+  gradientMap: "fiveTone",
+};
+material.gradientMap = fiveTone;
+
+const lightFolder = gui.addFolder("THREE.Light");
+lightFolder.addColor(data, "lightColor").onChange(() => {
+  light.color.setHex(Number(data.lightColor.toString().replace("#", "0x")));
+});
+lightFolder.add(light, "intensity", 0, 4);
+
+const materialFolder = gui.addFolder("THREE.Material");
+materialFolder.add(material, "transparent");
+materialFolder.add(material, "opacity", 0, 1, 0.01);
+materialFolder.add(material, "depthTest");
+materialFolder.add(material, "depthWrite");
+materialFolder
+  .add(material, "alphaTest", 0, 1, 0.01)
+  .onChange(() => updateMaterial());
+materialFolder.add(material, "visible");
+materialFolder
+  .add(material, "side", options.side)
+  .onChange(() => updateMaterial());
+//materialFolder.open()
+
+var meshToonMaterialFolder = gui.addFolder("THREE.MeshToonMaterial");
+meshToonMaterialFolder.addColor(data, "color").onChange(() => {
+  material.color.setHex(Number(data.color.toString().replace("#", "0x")));
+});
+meshToonMaterialFolder
+  .add(material, "flatShading")
+  .onChange(() => updateMaterial());
+meshToonMaterialFolder
+  .add(data, "gradientMap", options.gradientMap)
+  .onChange(() => updateMaterial());
+meshToonMaterialFolder.open();
+
+function updateMaterial() {
+  material.side = Number(material.side);
+  material.gradientMap = eval(data.gradientMap as any);
+  material.needsUpdate = true;
+}
 
 var animate = function () {
-    requestAnimationFrame(animate)
+  requestAnimationFrame(animate);
 
-    controls.update()
+  icosahedron.rotation.y += 0.005;
+  icosahedron.rotation.x += 0.005;
+  cube.rotation.y += 0.005;
+  cube.rotation.x += 0.005;
+  torusKnot.rotation.y += 0.005;
+  torusKnot.rotation.x += 0.005;
+  sphere.rotation.y += 0.005;
+  sphere.rotation.x += 0.005;
+  plane.rotation.y += 0.005;
+  plane.rotation.x += 0.005;
 
-    render()
+  render();
 
-    stats.update()
+  stats.update();
 };
 
 function render() {
-    renderer.render(scene, camera)
+  renderer.render(scene, camera);
 }
 animate();
